507a59cf1c85dbcd7467b8eec044a70be49b7681
RedBoy <arosioalex06@gmail.com>
2023-04-30 12:53:38 +0200

    Instant Fix

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const terminalInput = document.querySelector(".terminal-input");
 terminalInput.addEventListener('keypress', async (event) => {
   if (event.key !== 'Enter') return;
 
-  var output = getMessage(terminalInput.value).messages
+  var output = getMessage(terminalInput.value)
 
-  if (output == undefined) output = "Questo comando non esiste!"
   terminalInput.value = null
 
   await sleep(500)
 
-  output.map(async mess => {
+  if (output == undefined) {
+    terminalText.innerHTML += "~ ❯ Questo comando non esiste<br>"
+    return
+  }
+
+  output.messages.map(async mess => {
     terminalText.innerHTML += "~ ❯ " + mess + "<br>"
     //await sleep(250) Perchè non funzioni?
   })
